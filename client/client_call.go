83dacfdc4d1202add1f2c3b8bb71760062187c38
Gkirito <lownkid@outlook.com>
2023-01-16 15:01:37 +0800

    🎨 style(client/client_call): format

--- client/client_call.go
+++ client/client_call.go
@@ -84,56 +84,47 @@ func (c *Client) BatchGetFilteredObjectsOwnedByAddress(ctx context.Context, addr
 
 func (c *Client) BatchTransaction(ctx context.Context, signer types.Address, txnParams []map[string]interface{}, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_batchTransaction", signer, txnParams, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_batchTransaction", signer, txnParams, gas, gasBudget)
 }
 
 func (c *Client) DryRunTransaction(ctx context.Context, tx *types.TransactionBytes) (*types.TransactionEffects, error) {
 	resp := types.TransactionEffects{}
-	err := c.CallContext(ctx, &resp, "sui_dryRunTransaction", tx.TxBytes)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_dryRunTransaction", tx.TxBytes)
 }
 
 func (c *Client) ExecuteTransaction(ctx context.Context, txn types.SignedTransaction, requestType types.ExecuteTransactionRequestType) (*types.ExecuteTransactionResponse, error) {
 	resp := types.ExecuteTransactionResponse{}
-	err := c.CallContext(ctx, &resp, "sui_executeTransaction", txn.TxBytes, txn.SigScheme, txn.Signature, txn.PublicKey, requestType)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_executeTransaction", txn.TxBytes, txn.SigScheme, txn.Signature, txn.PublicKey, requestType)
 }
 
 func (c *Client) GetObject(ctx context.Context, objID types.ObjectId) (*types.ObjectRead, error) {
 	resp := types.ObjectRead{}
-	err := c.CallContext(ctx, &resp, "sui_getObject", objID)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_getObject", objID)
 }
 
 func (c *Client) GetObjectsOwnedByAddress(ctx context.Context, address types.Address) ([]types.ObjectInfo, error) {
 	var resp []types.ObjectInfo
-	err := c.CallContext(ctx, &resp, "sui_getObjectsOwnedByAddress", address)
-	return resp, err
+	return resp, c.CallContext(ctx, &resp, "sui_getObjectsOwnedByAddress", address)
 }
 
 func (c *Client) GetObjectsOwnedByObject(ctx context.Context, objID types.ObjectId) ([]types.ObjectInfo, error) {
 	var resp []types.ObjectInfo
-	err := c.CallContext(ctx, &resp, "sui_getObjectsOwnedByObject", objID)
-	return resp, err
+	return resp, c.CallContext(ctx, &resp, "sui_getObjectsOwnedByObject", objID)
 }
 
 func (c *Client) GetRawObject(ctx context.Context, objID types.ObjectId) (*types.ObjectRead, error) {
 	resp := types.ObjectRead{}
-	err := c.CallContext(ctx, &resp, "sui_getRawObject", objID)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_getRawObject", objID)
 }
 
 func (c *Client) GetTotalTransactionNumber(ctx context.Context) (uint64, error) {
 	resp := uint64(0)
-	err := c.CallContext(ctx, &resp, "sui_getTotalTransactionNumber")
-	return resp, err
+	return resp, c.CallContext(ctx, &resp, "sui_getTotalTransactionNumber")
 }
 
 func (c *Client) GetTransactionsInRange(ctx context.Context, start, end uint64) ([]string, error) {
 	var resp []string
-	err := c.CallContext(ctx, &resp, "sui_getTransactionsInRange", start, end)
-	return resp, err
+	return resp, c.CallContext(ctx, &resp, "sui_getTransactionsInRange", start, end)
 }
 
 func (c *Client) BatchGetTransaction(digests []string) (map[string]*types.TransactionResponse, error) {
@@ -150,10 +141,7 @@ func (c *Client) BatchGetTransaction(digests []string) (map[string]*types.Transa
 			Result: results[v],
 		})
 	}
-	if err := c.BatchCall(elems); err != nil {
-		return nil, err
-	}
-	return results, nil
+	return results, c.BatchCall(elems)
 }
 
 func (c *Client) BatchGetObject(objects []types.ObjectId) (map[string]*types.ObjectRead, error) {
@@ -170,23 +158,18 @@ func (c *Client) BatchGetObject(objects []types.ObjectId) (map[string]*types.Obj
 			Result: results[v.String()],
 		})
 	}
-	if err := c.BatchCall(elems); err != nil {
-		return nil, err
-	}
-	return results, nil
+	return results, c.BatchCall(elems)
 }
 
 func (c *Client) GetTransaction(ctx context.Context, digest string) (*types.TransactionResponse, error) {
 	resp := types.TransactionResponse{}
-	err := c.CallContext(ctx, &resp, "sui_getTransaction", digest)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_getTransaction", digest)
 }
 
 // MergeCoins Create an unsigned transaction to merge multiple coins into one coin.
 func (c *Client) MergeCoins(ctx context.Context, signer types.Address, primaryCoin, coinToMerge types.ObjectId, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_mergeCoins", signer, primaryCoin, coinToMerge, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_mergeCoins", signer, primaryCoin, coinToMerge, gas, gasBudget)
 }
 
 // MoveCall Create an unsigned transaction to execute a Move call on the network, by calling the specified function in the module of a given package.
@@ -194,67 +177,58 @@ func (c *Client) MergeCoins(ctx context.Context, signer types.Address, primaryCo
 // So now only methods with `typeArguments` are supported
 func (c *Client) MoveCall(ctx context.Context, signer types.Address, packageId types.ObjectId, module, function string, typeArgs []string, arguments []any, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_moveCall", signer, packageId, module, function, typeArgs, arguments, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_moveCall", signer, packageId, module, function, typeArgs, arguments, gas, gasBudget)
 }
 
 // SplitCoin Create an unsigned transaction to split a coin object into multiple coins.
 func (c *Client) SplitCoin(ctx context.Context, signer types.Address, Coin types.ObjectId, splitAmounts []uint64, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_splitCoin", signer, Coin, splitAmounts, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_splitCoin", signer, Coin, splitAmounts, gas, gasBudget)
 }
 
 // SplitCoinEqual Create an unsigned transaction to split a coin object into multiple equal-size coins.
 func (c *Client) SplitCoinEqual(ctx context.Context, signer types.Address, Coin types.ObjectId, splitCount uint64, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_splitCoinEqual", signer, Coin, splitCount, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_splitCoinEqual", signer, Coin, splitCount, gas, gasBudget)
 }
 
 // TransferObject Create an unsigned transaction to transfer an object from one address to another. The object's type must allow public transfers
 func (c *Client) TransferObject(ctx context.Context, signer, recipient types.Address, objID types.ObjectId, gas *types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_transferObject", signer, objID, gas, gasBudget, recipient)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_transferObject", signer, objID, gas, gasBudget, recipient)
 }
 
 // TransferSui Create an unsigned transaction to send SUI coin object to a Sui address. The SUI object is also used as the gas object.
 func (c *Client) TransferSui(ctx context.Context, signer, recipient types.Address, suiObjID types.ObjectId, amount, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_transferSui", signer, suiObjID, gasBudget, recipient, amount)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_transferSui", signer, suiObjID, gasBudget, recipient, amount)
 }
 
 // PayAllSui Create an unsigned transaction to send all SUI coins to one recipient.
 func (c *Client) PayAllSui(ctx context.Context, signer, recipient types.Address, inputCoins []types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_payAllSui", signer, inputCoins, recipient, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_payAllSui", signer, inputCoins, recipient, gasBudget)
+
 }
 
 func (c *Client) GetCoinMetadata(ctx context.Context, coinType string) (*types.SuiCoinMetadata, error) {
 	resp := types.SuiCoinMetadata{}
-	err := c.CallContext(ctx, &resp, "sui_getCoinMetadata", coinType)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_getCoinMetadata", coinType)
 }
 
 func (c *Client) Pay(ctx context.Context, signer types.Address, inputCoins []types.ObjectId, recipients []types.Address, amount []uint64, gas types.ObjectId, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_pay", signer, inputCoins, recipients, amount, gas, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_pay", signer, inputCoins, recipients, amount, gas, gasBudget)
 }
 
 func (c *Client) PaySui(ctx context.Context, signer types.Address, inputCoins []types.ObjectId, recipients []types.Address, amount []uint64, gasBudget uint64) (*types.TransactionBytes, error) {
 	resp := types.TransactionBytes{}
-	err := c.CallContext(ctx, &resp, "sui_paySui", signer, inputCoins, recipients, amount, gasBudget)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_paySui", signer, inputCoins, recipients, amount, gasBudget)
 }
 
 func (c *Client) GetAllBalances(ctx context.Context, address types.Address) ([]types.SuiCoinBalance, error) {
 	var resp []types.SuiCoinBalance
-	err := c.CallContext(ctx, &resp, "sui_getAllBalances", address)
-	return resp, err
+	return resp, c.CallContext(ctx, &resp, "sui_getAllBalances", address)
 }
 
 // GetBalance to use default sui coin(0x2::sui::SUI) when coinType is nil
@@ -265,14 +239,12 @@ func (c *Client) GetBalance(ctx context.Context, address types.Address, coinType
 
 func (c *Client) DevInspectMoveCall(ctx context.Context, signer types.Address, packageId types.ObjectId, module, function string, typeArgs []string, arguments []any) (*types.DevInspectResults, error) {
 	var resp types.DevInspectResults
-	err := c.CallContext(ctx, &resp, "sui_devInspectMoveCall", signer, packageId, module, function, typeArgs, arguments)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_devInspectMoveCall", signer, packageId, module, function, typeArgs, arguments)
 }
 
 func (c *Client) DevInspectTransaction(ctx context.Context, txByte types.Base64Data) (*types.DevInspectResults, error) {
 	var resp types.DevInspectResults
-	err := c.CallContext(ctx, &resp, "sui_devInspectTransaction", txByte)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_devInspectTransaction", txByte)
 }
 
 // GetCoins to use default sui coin(0x2::sui::SUI) when coinType is nil
@@ -296,8 +268,7 @@ func (c *Client) GetTotalSupply(ctx context.Context, coinType string) (*types.Su
 
 func (c *Client) ExecuteTransactionSerializedSig(ctx context.Context, txn types.SignedTransactionSerializedSig, requestType types.ExecuteTransactionRequestType) (*types.ExecuteTransactionResponse, error) {
 	resp := types.ExecuteTransactionResponse{}
-	err := c.CallContext(ctx, &resp, "sui_executeTransactionSerializedSig", txn.TxBytes, txn.Signature, requestType)
-	return &resp, err
+	return &resp, c.CallContext(ctx, &resp, "sui_executeTransactionSerializedSig", txn.TxBytes, txn.Signature, requestType)
 }
 
 /*

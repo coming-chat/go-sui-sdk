40c0dac93fea489881b2af0a5baa2539888d4dbf
Gkirito <lownkid@outlook.com>
2023-01-30 17:16:52 +0800

    🧪 test(types/types_test): add transactionQuery test case

--- types/types_test.go
+++ types/types_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -52,5 +53,46 @@ func TestObjectOwnerJsonENDE(t *testing.T) {
 
 	if !bytes.Equal(jsonString2, enData2) {
 		t.Fatal("encode failed")
+	}
+}
+
+func TestTransactionQuery_MarshalJSON(t1 *testing.T) {
+	type fields struct {
+		All           *string
+		MoveFunction  *MoveFunction
+		InputObject   *ObjectId
+		MutatedObject *ObjectId
+		FromAddress   *Address
+		ToAddress     *Address
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []byte
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "test1",
+			fields: fields{
+				FromAddress: AddressFromHex(t1, "0x6fc6148816617c3c3eccb1d09e930f73f6712c9c"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := TransactionQuery{
+				All:           tt.fields.All,
+				MoveFunction:  tt.fields.MoveFunction,
+				InputObject:   tt.fields.InputObject,
+				MutatedObject: tt.fields.MutatedObject,
+				FromAddress:   tt.fields.FromAddress,
+				ToAddress:     tt.fields.ToAddress,
+			}
+			got, err := json.Marshal(t)
+			if !tt.wantErr(t1, err, fmt.Sprintf("MarshalJSON()")) {
+				return
+			}
+			assert.Equalf(t1, tt.want, got, "MarshalJSON()")
+		})
 	}
 }

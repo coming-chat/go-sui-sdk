567ab42ad6522bff3a7bb9545be3a9b5e698a027
Gkirito <lownkid@outlook.com>
2023-01-31 11:34:35 +0800

    🧪 test(account/account_test): remove sensitive logs

--- account/account_test.go
+++ account/account_test.go
@@ -13,7 +13,5 @@ func TestMyAccouunt(t *testing.T) {
 	account, err := NewAccountWithMnemonic(Mnemonic)
 	assert.Nil(t, err)
 
-	t.Logf("pri = %x", account.PrivateKey[:32])
-	t.Logf("pub = %x", account.PublicKey)
-	t.Logf("addr = %v", account.Address) // 0x6c5d2cd6e62734f61b4e318e58cbfd1c4b99dfaf
+	t.Logf("addr = %v", account.Address)
 }

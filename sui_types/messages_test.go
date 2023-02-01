6cd5c6acfa94bc117c63acd97ab1ac17cbfaaddc
Gkirito <lownkid@outlook.com>
2023-02-01 15:35:39 +0800

    🧪 test(sui_types/messages_test): add new test for encode transaction offline

---
+++ sui_types/messages_test.go
@@ -0,0 +1,122 @@
+package sui_types
+
+import (
+	"bytes"
+	"context"
+	"encoding/hex"
+	"github.com/coming-chat/go-sui/client"
+	"github.com/coming-chat/go-sui/types"
+	"github.com/fardream/go-bcs/bcs"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"os"
+	"testing"
+)
+
+var (
+	Address, _ = types.NewAddressFromHex("0xb08ae4d187ca0057baa1666fe43fb9d7f3693a9a")
+	M1Mnemonic = os.Getenv("WalletSdkTestM1")
+)
+
+func Test_BCSEncodeTransactionData(t *testing.T) {
+	chain, err := client.Dial(types.DevNetRpcUrl)
+	require.NoError(t, err)
+	coins, err := chain.GetSuiCoinsOwnedByAddress(context.TODO(), *Address)
+	require.NoError(t, err)
+	transferCoins, coin, err := coins.PickSUICoinsWithGas(big.NewInt(1000), 1000, types.PickByOrder)
+	require.NoError(t, err)
+	var (
+		coinRef = []*types.ObjectRef{coin.Reference}
+		coinId  = []types.ObjectId{coin.Reference.ObjectId}
+	)
+	for _, v := range transferCoins {
+		coinRef = append(coinRef, v.Reference)
+		coinId = append(coinId, v.Reference.ObjectId)
+	}
+	tx := TransactionData{
+		Kind: TransactionKind{
+			Single: &SingleTransactionKind{
+				PayAllSui: &PayAllSui{
+					Coins:     coinRef,
+					Recipient: *Address,
+				},
+			},
+		},
+		Sender:     *Address,
+		GasPayment: *coin.Reference,
+		GasPrice:   uint64(1),
+		GasBudget:  uint64(1000),
+	}
+	encodeTx, err := bcs.Marshal(tx)
+
+	currentTxEncode, err := chain.PayAllSui(context.TODO(), *Address, *Address, coinId, 1000)
+	require.NoError(t, err)
+	t.Logf("%x", encodeTx)
+	t.Logf("%x", currentTxEncode.TxBytes.Data())
+	if !bytes.Equal(encodeTx, currentTxEncode.TxBytes.Data()) {
+		t.Fatal("encode failed")
+	}
+}
+
+func TestBCS_EncodeMoveCall(t *testing.T) {
+	chain, err := client.Dial(types.DevNetRpcUrl)
+	require.NoError(t, err)
+	coins, err := chain.GetSuiCoinsOwnedByAddress(context.TODO(), *Address)
+	require.NoError(t, err)
+	coin, err := coins.PickCoinNoLess(2000)
+	require.NoError(t, err)
+	packageId, err := types.NewHexData("0xc84f497dc6dbbfc9d4dc6dea62d22f90c320a8f3")
+	require.NoError(t, err)
+	packageRead, err := chain.GetObject(context.TODO(), *packageId)
+	require.NoError(t, err)
+	globalProfile, err := types.NewHexData("0xce07c42b29205952c22b817c0baa2dad8391a688")
+	require.NoError(t, err)
+	profile := "{\"name\":\"test\",\"bio\":\"Hello\",\"avatar\":\"\"}"
+	signature, err := hex.DecodeString("80c90affb399de8f7c372a6f3da435fa99a1cda259359ca75956874bbfa5f88a3b02b204a5b127c2f1e6ea94936865683a1ac9652e63d307bbac003a0724ce0e")
+	require.NoError(t, err)
+	profileBcsEn, err := bcs.Marshal([]byte(profile))
+	require.NoError(t, err)
+	signatureBcsEn, err := bcs.Marshal(signature)
+	require.NoError(t, err)
+	tx := TransactionData{
+		Kind: TransactionKind{
+			Single: &SingleTransactionKind{
+				Call: &MoveCall{
+					Package:       *packageRead.Details.Reference,
+					Module:        "profile",
+					Function:      "register",
+					TypeArguments: []*TypeTag{},
+					Arguments: []*CallArg{
+						{
+							Object: &ObjectArg{
+								SharedObject: &SharedObject{
+									Id:                   *globalProfile,
+									InitialSharedVersion: 1181,
+								},
+							},
+						},
+						{
+							Pure: &profileBcsEn,
+						},
+						{
+							Pure: &signatureBcsEn,
+						},
+					},
+				},
+			},
+		},
+		Sender:     *Address,
+		GasPayment: *coin.Reference,
+		GasPrice:   uint64(1),
+		GasBudget:  uint64(2000),
+	}
+	encodeTx, err := bcs.Marshal(tx)
+	require.NoError(t, err)
+	currentTxEncode, err := chain.MoveCall(context.TODO(), *Address, *packageId, "profile", "register", []string{}, []any{globalProfile, profile, "0xd485020c6ac369e6f2b28be2dcca24ebfd827c53893b6462e9e65cf16dba3cedf004e8740b8c8c3579a4391269b9e103bcfc39627c6af729abb7675bc8004301"}, &coin.Reference.ObjectId, 2000)
+	require.NoError(t, err)
+	t.Logf("%x", encodeTx)
+	t.Logf("%x", currentTxEncode.TxBytes.Data())
+	if !bytes.Equal(encodeTx, currentTxEncode.TxBytes.Data()) {
+		t.Fatal("encode failed")
+	}
+}

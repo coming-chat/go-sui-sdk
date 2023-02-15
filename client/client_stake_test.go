4688d6d6255da62e8ffa63da295f6ecc0e697236
GG <863867759@qq.com>
2023-02-15 11:29:22 +0800

    test: stake add/switch/withdraw testcase.

--- client/client_stake_test.go
+++ client/client_stake_test.go
@@ -5,6 +5,7 @@ import (
 
 	"testing"
 
+	"github.com/coming-chat/go-sui/account"
 	"github.com/coming-chat/go-sui/types"
 	"github.com/stretchr/testify/require"
 )
@@ -51,107 +52,135 @@ func TestGetSuiSystemState(t *testing.T) {
 	}
 }
 
-// need to update
-//func TestRequestAddDelegation(t *testing.T) {
-//	cli := TestnetClient(t)
-//
-//	acc := M1Account(t)
-//	addr, _ := types.NewAddressFromHex(acc.Address)
-//
-//	coin1 := "0x6619dfd5b9d955f7734ef1e680a222f40861302b" // 0.02
-//	coin2 := "0x7b2bb7b953a9529aa8b6ca267d8d20aa5113d92e" // 0.02 // 20000000
-//	c1, _ := types.NewHexData(coin1)
-//	c2, _ := types.NewHexData(coin2)
-//	coins := []types.ObjectId{*c1, *c2}
-//	amount := uint64(20000000 * 2)
-//
-//	validatorAddress := "0xc397cc2c83165c78a850a0295fb08538de9566f1"
-//	validator, _ := types.NewAddressFromHex(validatorAddress)
-//
-//	gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
-//	gas, _ := types.NewHexData(gasId)
-//
-//	txn, err := cli.RequestAddDelegation(context.Background(), *addr, coins, amount, *validator, *gas, 20000)
-//	require.Nil(t, err)
-//
-//	// simulate
-//	simulate, err := cli.DryRunTransaction(context.Background(), txn)
-//	require.Nil(t, err)
-//	t.Log(simulate)
-//
-//	require.Equal(t, simulate.Status.Status, types.TransactionStatusSuccess)
-//
-//	signedTxn := txn.SignSerializedSigWith(acc.PrivateKey)
-//	resp, err := cli.ExecuteTransactionSerializedSig(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
-//	require.Nil(t, err)
-//	t.Log(resp)
-//
-//	// result: https://explorer.sui.io/transaction/EcH9dK1wSLzgv15CNNHr2KvDEpARUW5mSeug3LHeFqGB?network=testnet
-//}
-//
-//func TestRequestSwitchDelegation(t *testing.T) {
-//	cli := TestnetClient(t)
-//
-//	acc := M1Account(t)
-//	addr, _ := types.NewAddressFromHex(acc.Address)
-//
-//	// queryed actived delegated stake
-//	// actived status = DelegatedStake.DelegationStatus (ActiveDelegationStatus)
-//	delegationId := "0x7c6d82f689221d2763df7f4768be692bb684cb6f" // status.Id.Id
-//	delegation, _ := types.NewHexData(delegationId)
-//	stakedSuiId := "0x2b474dc1df5d995d2e45af2e16ac5f9fff57cf72" // status.StakedSuiId
-//	stakedID, _ := types.NewHexData(stakedSuiId)
-//
-//	newValidator := "0xfdc2a0fe740d34e5424d1a06c0c4ac106b7096f2"
-//	newAddr, _ := types.NewAddressFromHex(newValidator)
-//
-//	gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
-//	gas, _ := types.NewHexData(gasId)
-//
-//	txn, err := cli.RequestSwitchDelegation(context.Background(), *addr, *delegation, *stakedID, *newAddr, *gas, 20000)
-//	require.Nil(t, err)
-//
-//	// simulate
-//	simulate, err := cli.DryRunTransaction(context.Background(), txn)
-//	require.Nil(t, err)
-//	require.Equal(t, simulate.Status.Status, types.TransactionStatusSuccess)
-//
-//	signedTxn := txn.SignSerializedSigWith(acc.PrivateKey)
-//	resp, err := cli.ExecuteTransactionSerializedSig(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
-//	require.Nil(t, err)
-//	t.Log(resp)
-//
-//	// result: https://explorer.sui.io/transaction/AV5fTh8RwV4nMUunJkWuN5C7HgfosR8MHHAaKWTcT768?network=testnet
-//}
-//
-//func TestRequestWithdrawDelegation(t *testing.T) {
-//	cli := TestnetClient(t)
-//
-//	acc := M1Account(t)
-//	addr, _ := types.NewAddressFromHex(acc.Address)
-//
-//	// queryed actived delegated stake
-//	// actived status = DelegatedStake.DelegationStatus (ActiveDelegationStatus)
-//	delegationId := "0x012f0a2c86035d5f31d375de877a1c20220260ef" // status.Id.Id
-//	delegation, _ := types.NewHexData(delegationId)
-//	stakedSuiId := "0x8ad8686c34668aca5d3efa041ca1a10986d332a6" // status.StakedSuiId
-//	stakedID, _ := types.NewHexData(stakedSuiId)
-//
-//	gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
-//	gas, _ := types.NewHexData(gasId)
-//
-//	txn, err := cli.RequestWithdrawDelegation(context.Background(), *addr, *delegation, *stakedID, *gas, 20000)
-//	require.Nil(t, err)
-//
-//	// simulate
-//	simulate, err := cli.DryRunTransaction(context.Background(), txn)
-//	require.Nil(t, err)
-//	require.Equal(t, simulate.Status.Status, types.TransactionStatusSuccess)
-//
-//	signedTxn := txn.SignSerializedSigWith(acc.PrivateKey)
-//	resp, err := cli.ExecuteTransactionSerializedSig(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
-//	require.Nil(t, err)
-//	t.Log(resp)
-//
-//	// result: https://explorer.sui.io/transaction/DVpYBQ8Djg7jtHhcAwy8hgeWCjRpTaTYkNzpNigco5HP?network=testnet
-//}
+func TestRequestAddDelegation(t *testing.T) {
+	if false {
+		coins := []string{
+			"0x6619dfd5b9d955f7734ef1e680a222f40861302b", // 20000000 // 0.02
+			"0x7b2bb7b953a9529aa8b6ca267d8d20aa5113d92e", // 20000000
+		}
+		amount := uint64(20000000 * 2) // all use
+		validatorAddress := "0xc397cc2c83165c78a850a0295fb08538de9566f1"
+		gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
+		requestAddDelegation(t, coins, amount, validatorAddress, gasId)
+		// ✅ https://explorer.sui.io/transaction/EcH9dK1wSLzgv15CNNHr2KvDEpARUW5mSeug3LHeFqGB?network=testnet
+	}
+
+	if false {
+		coins := []string{
+			"0x82ba777672c570959a4a25b076bf2b80dfefd367", // 20000000 // 0.02
+			"0x838b3d559569a132f0f86d13883826651105ecc7", // 20000000
+		}
+		amount := uint64(30000000) // only use 0.03
+		validatorAddress := "0xc397cc2c83165c78a850a0295fb08538de9566f1"
+		gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
+		requestAddDelegation(t, coins, amount, validatorAddress, gasId)
+		// ✅ https://explorer.sui.io/transaction/F3VXATTbZU2VtrJv7V95FRkAQ4KAwBqr3mZHmcPRs3fC?network=testnet
+	}
+}
+
+func TestRequestSwitchDelegation(t *testing.T) {
+	if false {
+		delegationId := "0x7c6d82f689221d2763df7f4768be692bb684cb6f" // status.Id.Id
+		stakedSuiId := "0x2b474dc1df5d995d2e45af2e16ac5f9fff57cf72"  // status.StakedSuiId
+		newValidator := "0xfdc2a0fe740d34e5424d1a06c0c4ac106b7096f2"
+		gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
+		requestSwitchDelegation(t, delegationId, stakedSuiId, newValidator, gasId)
+		// ✅ https://explorer.sui.io/transaction/AV5fTh8RwV4nMUunJkWuN5C7HgfosR8MHHAaKWTcT768?network=testnet
+	}
+}
+
+func TestRequestWithdrawDelegation(t *testing.T) {
+	if false {
+		delegationId := "0x012f0a2c86035d5f31d375de877a1c20220260ef" // status.Id.Id
+		stakedSuiId := "0x8ad8686c34668aca5d3efa041ca1a10986d332a6"  // status.StakedSuiId
+		gasId := "0x3a1afb6e982cec8b9d5971f0e70fd53ae3e4cd4f"
+		requestWithdrawDelegation(t, delegationId, stakedSuiId, gasId)
+		// ✅ https://explorer.sui.io/transaction/DVpYBQ8Djg7jtHhcAwy8hgeWCjRpTaTYkNzpNigco5HP?network=testnet
+	}
+}
+
+func requestAddDelegation(t *testing.T, coinIds []string, amount uint64, validatorAddress string, gasId string) {
+	cli := TestnetClient(t)
+	acc := M1Account(t)
+	addr, _ := types.NewAddressFromHex(acc.Address)
+
+	var coins = []types.ObjectId{}
+	for _, id := range coinIds {
+		coin, err := types.NewHexData(id)
+		require.Nil(t, err)
+		coins = append(coins, *coin)
+	}
+
+	validator, err := types.NewAddressFromHex(validatorAddress)
+	require.Nil(t, err)
+
+	gas, err := types.NewHexData(gasId)
+	require.Nil(t, err)
+
+	txn, err := cli.RequestAddDelegation(context.Background(), *addr, coins, amount, *validator, *gas, 20000)
+	require.Nil(t, err)
+
+	resp := simulateAndSendTxn(t, cli, txn, acc)
+	t.Log(resp)
+}
+
+func requestSwitchDelegation(t *testing.T, delegationId, stakedId, validator, gasId string) {
+	cli := TestnetClient(t)
+	acc := M1Account(t)
+	addr, _ := types.NewAddressFromHex(acc.Address)
+
+	// queryed actived delegated stake
+	// actived status = DelegatedStake.DelegationStatus (ActiveDelegationStatus)
+	delegation, err := types.NewHexData(delegationId) // status.Id.Id
+	require.Nil(t, err)
+	stakedID, err := types.NewHexData(stakedId) // status.StakedSuiId
+	require.Nil(t, err)
+
+	newValidator, err := types.NewAddressFromHex(validator)
+	require.Nil(t, err)
+
+	gas, err := types.NewHexData(gasId)
+	require.Nil(t, err)
+
+	txn, err := cli.RequestSwitchDelegation(context.Background(), *addr, *delegation, *stakedID, *newValidator, *gas, 20000)
+	require.Nil(t, err)
+
+	resp := simulateAndSendTxn(t, cli, txn, acc)
+	t.Log(resp)
+}
+
+func requestWithdrawDelegation(t *testing.T, delegationId, stakedId, gasId string) {
+	cli := TestnetClient(t)
+	acc := M1Account(t)
+	addr, _ := types.NewAddressFromHex(acc.Address)
+
+	// queryed actived delegated stake
+	// actived status = DelegatedStake.DelegationStatus (ActiveDelegationStatus)
+	delegation, err := types.NewHexData(delegationId) // status.Id.Id
+	require.Nil(t, err)
+	stakedID, err := types.NewHexData(stakedId) // status.StakedSuiId
+	require.Nil(t, err)
+
+	gas, err := types.NewHexData(gasId)
+	require.Nil(t, err)
+
+	txn, err := cli.RequestWithdrawDelegation(context.Background(), *addr, *delegation, *stakedID, *gas, 20000)
+	require.Nil(t, err)
+
+	resp := simulateAndSendTxn(t, cli, txn, acc)
+	t.Log(resp)
+}
+
+func simulateAndSendTxn(t *testing.T, cli *Client, txn *types.TransactionBytes, acc *account.Account) *types.ExecuteTransactionResponse {
+	// simulate
+	simulate, err := cli.DryRunTransaction(context.Background(), txn)
+	require.Nil(t, err)
+	t.Log(simulate)
+
+	require.Equal(t, simulate.Status.Status, types.TransactionStatusSuccess)
+
+	signedTxn := txn.SignSerializedSigWith(acc.PrivateKey)
+	resp, err := cli.ExecuteTransactionSerializedSig(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
+	require.Nil(t, err)
+	return resp
+}

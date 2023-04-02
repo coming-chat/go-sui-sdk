133168668826a9c3562a52b61e4875b9072e3890
Gkirito <lownkid@outlook.com>
2023-04-02 20:37:14 +0800

    feat(account): rename func

--- account/account.go
+++ account/account.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"github.com/coming-chat/go-aptos/crypto/derivation"
 	"github.com/coming-chat/go-sui/sui_types"
+	"github.com/coming-chat/go-sui/types"
 	"github.com/tyler-smith/go-bip39"
 	"golang.org/x/crypto/blake2b"
 )
@@ -68,8 +69,8 @@ func (a *Account) Sign(data []byte) []byte {
 	}
 }
 
-func (a *Account) SignSecure(msg any, intent sui_types.Intent) (sui_types.Signature, error) {
-	signature, err := sui_types.NewSignatureSecure(
+func (a *Account) SignSecureWithoutEncode(msg types.Base64Data, intent sui_types.Intent) (sui_types.Signature, error) {
+	signature, err := sui_types.NewSignatureSecure[types.Base64Data](
 		sui_types.NewIntentMessage(intent, msg), &a.KeyPair,
 	)
 	if err != nil {

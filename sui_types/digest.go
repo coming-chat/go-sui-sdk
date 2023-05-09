f8be7b7d4db1617f189aae5fb29f7b426dd5ca8d
Gkirito <lownkid@outlook.com>
2023-05-09 18:43:02 +0800

    feat(sui_types): add digest

--- sui_types/digest.go
+++ sui_types/digest.go
@@ -2,6 +2,18 @@ package sui_types
 
 import "github.com/coming-chat/go-sui/lib"
 
-type Digest = lib.Base64Data
+type Digest = lib.Base58
 
 type ObjectDigest = Digest
+
+type TransactionDigest = Digest
+
+type TransactionEffectsDigest = Digest
+
+type TransactionEventsDigest = Digest
+
+type CheckpointDigest = Digest
+
+type CertificateDigest = Digest
+
+type CheckpointContentsDigest = Digest

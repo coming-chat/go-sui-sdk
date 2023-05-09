143d3fd3e18e3a137293c04cbef8171ca15ce049
Gkirito <lownkid@outlook.com>
2023-05-09 18:47:01 +0800

    refactor(types): delete unused type

--- types/common.go
+++ types/common.go
@@ -1,21 +1,10 @@
 package types
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
-	"strings"
-
+	"github.com/coming-chat/go-sui/sui_types"
 	"github.com/shopspring/decimal"
 )
-
-type TransactionDigest = string
-
-type TransactionEffectsDigest = string
-
-type TransactionEventDigest = string
-
-type SequenceNumber = uint64
 
 type SuiBigInt = decimal.Decimal
 
@@ -72,18 +61,18 @@ func (s *SafeSuiBigInt[T]) Decimal() decimal.Decimal {
 	return decimal.NewFromInt(s.Int64())
 }
 
-// export const ObjectId = string();
-// export type ObjectId = Infer<typeof ObjectId>;
+// export const ObjectID = string();
+// export type ObjectID = Infer<typeof ObjectID>;
 
 // export const SuiAddress = string();
 // export type SuiAddress = Infer<typeof SuiAddress>;
 
 type ObjectOwnerInternal struct {
-	AddressOwner *Address `json:"AddressOwner,omitempty"`
-	ObjectOwner  *Address `json:"ObjectOwner,omitempty"`
-	SingleOwner  *Address `json:"SingleOwner,omitempty"`
+	AddressOwner *sui_types.SuiAddress `json:"AddressOwner,omitempty"`
+	ObjectOwner  *sui_types.SuiAddress `json:"ObjectOwner,omitempty"`
+	SingleOwner  *sui_types.SuiAddress `json:"SingleOwner,omitempty"`
 	Shared       *struct {
-		InitialSharedVersion SequenceNumber `json:"initial_shared_version"`
+		InitialSharedVersion *sui_types.SequenceNumber `json:"initial_shared_version"`
 	} `json:"Shared,omitempty"`
 }
 
@@ -92,59 +81,8 @@ type ObjectOwner struct {
 	*string
 }
 
-type TagJsonType interface {
-	Tag() string
-	Content() string
-}
-
-type TagJson[T TagJsonType] struct {
-	Data T
-}
-
-func (t *TagJson[T]) UnmarshalJSON(data []byte) error {
-	tmp := make(map[string]json.RawMessage)
-	err := json.Unmarshal(data, &tmp)
-	if err != nil {
-		return err
-	}
-	rv := reflect.ValueOf(t).Elem().Field(0)
-	v, ok := tmp[t.Data.Tag()]
-	if !ok {
-		return fmt.Errorf("no such tag: %s in json data %v", t.Data.Tag(), tmp)
-	}
-	var subType string
-	err = json.Unmarshal(v, &subType)
-	if err != nil {
-		return fmt.Errorf("the tag [%s] value is not string", t.Data.Tag())
-	}
-	for i := 0; i < rv.Type().NumField(); i++ {
-		if !strings.Contains(rv.Type().Field(i).Tag.Get("json"), subType) {
-			continue
-		}
-		if rv.Field(i).Kind() != reflect.Pointer {
-			return fmt.Errorf("field %s not pointer", rv.Field(i).Type().Name())
-		}
-		if rv.Field(i).IsNil() {
-			rv.Field(i).Set(reflect.New(rv.Field(i).Type().Elem()))
-		}
-		jsonData := data
-		if t.Data.Content() != "" {
-			jsonData, ok = tmp[t.Data.Content()]
-			if !ok {
-				return fmt.Errorf("json data [%v] get content key [%s] failed", tmp, t.Data.Content())
-			}
-		}
-		err = json.Unmarshal(jsonData, rv.Field(i).Interface())
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-	return fmt.Errorf("no tag[%s] value <%s> in struct fields", t.Data.Tag(), v)
-}
-
 type Page[T SuiTransactionBlockResponse | SuiEvent | Coin | SuiObjectResponse | DynamicFieldInfo,
-	C TransactionDigest | EventId | ObjectId] struct {
+	C sui_types.TransactionDigest | EventId | sui_types.ObjectID] struct {
 	Data        []T  `json:"data"`
 	NextCursor  *C   `json:"nextCursor,omitempty"`
 	HasNextPage bool `json:"hasNextPage"`

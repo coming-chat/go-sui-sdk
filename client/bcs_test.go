01bacbdfb88f4d9ed6987893d1cfcbc0f99d69f8
GG <863867759@qq.com>
2023-05-15 17:45:55 +0800

    test: add case MoveCall(request_add_stake).

--- client/bcs_test.go
+++ client/bcs_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/coming-chat/go-sui/lib"
 	"github.com/coming-chat/go-sui/sui_types"
+	"github.com/coming-chat/go-sui/sui_types/sui_system_state"
 	"github.com/coming-chat/go-sui/types"
 	"github.com/fardream/go-bcs/bcs"
 	"github.com/stretchr/testify/require"
@@ -217,6 +219,94 @@ func TestBCS_Pay(t *testing.T) {
 
 	// XXX: Fail when there are multiple recipients
 	// require.Equal(t, txBytesBCS, txBytesRemote)
+}
+
+func TestBCS_MoveCall(t *testing.T) {
+	sender, err := sui_types.NewAddressFromHex("0xd77955e670f42c1bc5e94b9e68e5fe9bdbed9134d784f2a14dfe5fc1b24b5d9f")
+	require.NoError(t, err)
+	gasBudget := SUI(0.02).Uint64()
+	gasPrice := uint64(1000)
+
+	cli := TestnetClient(t)
+	coins := GetCoins(t, cli, *sender, 2)
+	coin, coin2 := coins[0], coins[1]
+
+	validatorAddress, err := sui_types.NewAddressFromHex(ComingChatValidatorAddress)
+	require.NoError(t, err)
+
+	// build with BCS
+	ptb := sui_types.NewProgrammableTransactionBuilder()
+
+	// case 1: split target amount
+	amtArg, err := ptb.Pure(SUI(1).Uint64())
+	require.NoError(t, err)
+	arg1 := ptb.Command(sui_types.Command{
+		SplitCoins: &struct {
+			Argument  sui_types.Argument
+			Arguments []sui_types.Argument
+		}{
+			Argument:  sui_types.Argument{GasCoin: &lib.EmptyEnum{}},
+			Arguments: []sui_types.Argument{amtArg},
+		},
+	}) // the coin is split result argument
+	arg2, err := ptb.Pure(validatorAddress)
+	require.NoError(t, err)
+	arg0, err := ptb.Obj(sui_types.SuiSystemMutObj)
+	require.NoError(t, err)
+	ptb.Command(sui_types.Command{
+		MoveCall: &sui_types.ProgrammableMoveCall{
+			Package:  *sui_types.SuiSystemAddress,
+			Module:   sui_system_state.SuiSystemModuleName,
+			Function: sui_types.AddStakeFunName,
+			Arguments: []sui_types.Argument{
+				arg0, arg1, arg2,
+			},
+		},
+	})
+	pt := ptb.Finish()
+	tx := sui_types.NewProgrammable(
+		*sender, []*sui_types.ObjectRef{
+			coin.Reference(),
+			coin2.Reference(),
+		},
+		pt, gasBudget, gasPrice,
+	)
+
+	// case 2: direct stake the specified coin
+	// coinArg := sui_types.CallArg{
+	// 	Object: &sui_types.ObjectArg{
+	// 		ImmOrOwnedObject: coin.Reference(),
+	// 	},
+	// }
+	// addrBytes := validatorAddress.Data()
+	// addrArg := sui_types.CallArg{
+	// 	Pure: &addrBytes,
+	// }
+	// err = ptb.MoveCall(
+	// 	*sui_types.SuiSystemAddress,
+	// 	sui_system_state.SuiSystemModuleName,
+	// 	sui_types.AddStakeFunName,
+	// 	[]move_types.TypeTag{},
+	// 	[]sui_types.CallArg{
+	// 		sui_types.SuiSystemMut,
+	// 		coinArg,
+	// 		addrArg,
+	// 	},
+	// )
+	// require.NoError(t, err)
+	// pt := ptb.Finish()
+	// tx := sui_types.NewProgrammable(
+	// 	*sender, []*sui_types.ObjectRef{
+	// 		coin2.Reference(),
+	// 	},
+	// 	pt, gasBudget, gasPrice,
+	// )
+
+	// build & simulate
+	txBytesBCS, err := bcs.Marshal(tx)
+	require.NoError(t, err)
+	resp := simulateCheck(t, cli, txBytesBCS, true)
+	t.Log(resp.Effects.Data.GasFee())
 }
 
 func GetCoins(t *testing.T, cli *Client, sender suiAddress, needCount int) []types.Coin {
